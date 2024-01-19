64c2198fd92ba96b511cf7ea94916cc5e3664815
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-19 17:49:07 +0100

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,15 +1,18 @@
 import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 import React from "react";
 
-
 const ColorModeSwitch = () => {
-    const {colorMode, toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
-        <Switch colorScheme="green" isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-        <Text>Dark Mode</Text>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
-  )
+  );
 };
 
 export default ColorModeSwitch;
